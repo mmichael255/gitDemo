a4f2f29106852e204d5f91445f165352f68f8cab
michael <cby371@gmail.com>
2020-01-01 23:22:46 +0800

    增加了第二

--- git/src/main/java/com/chenbaiyu/git/demo/GitDemo.java
+++ git/src/main/java/com/chenbaiyu/git/demo/GitDemo.java
@@ -3,5 +3,6 @@ package com.chenbaiyu.git.demo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("first gitman");
+        System.out.println("second");
     }
 }
